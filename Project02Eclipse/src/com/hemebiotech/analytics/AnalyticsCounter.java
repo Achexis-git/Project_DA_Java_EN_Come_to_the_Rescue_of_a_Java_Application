cfe7a3759e1754490f790db9e912380450d0d843
Alexis Rouvroy <rouvroy.alexis@gmail.com>
2021-07-16 18:22:15 +0200

    Add javadocs

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,16 +2,33 @@ package com.hemebiotech.analytics;
 
 import java.util.List;
 
+/**
+ * Main class
+ * 
+ * @version 2.0
+ * @author Alexis Rouvroy
+ *
+ */
 public class AnalyticsCounter {
-	
-	public static void main(String args[]) throws Exception {	
+
+	/**
+	 * Main method that create and use two objects :
+	 * <ul>
+	 * <li>First class read the symptoms file and create a list of Strings</li>
+	 * <li>Second class count the occurrences and write the result on a file</li>
+	 * </ul>
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String args[]) throws Exception {
 		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		List<String> symptoms = reader.GetSymptoms();
-		
+
 		WriteResultsInFile writer = new WriteResultsInFile("Project02Eclipse/result.out", symptoms);
-		
+
 		writer.countSymptomsAndWriteFile();
-		
-		System.out.println("end");
+
+		System.out.println("The end");
 	}
 }
